10e93f6c73284904344c89788026df539f041933
debmallya <debmallyapathak2017@gmail.com>
2024-03-09 19:20:56 +0530

    first commit

---
+++ react-client/src/components/core/auth/SignupForm.jsx
@@ -0,0 +1,181 @@
+import { useState } from "react"
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
+import IconBtn from "../../common/IconBtn"
+import { signUp } from "../../../services/operations/authOperations"
+import { useNavigate } from "react-router-dom"
+import toast from "react-hot-toast"
+import { useDispatch } from "react-redux"
+
+function SignupForm() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+
+  const [formData, setFormData] = useState({
+    username:"",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  })
+
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+
+  const { username , firstName, lastName, email, password, confirmPassword } = formData
+
+  // Handle input fields, when some value changes
+  const handleOnChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  // Handle Form Submission
+  const handleOnSubmit = async(e) => {
+    e.preventDefault()
+
+    if (password !== confirmPassword) {
+      toast.error("Passwords Do Not Match")
+      return
+    }
+    console.log(formData)
+ 
+      const resp =  dispatch(signUp(username , firstName, lastName, email, password, confirmPassword,navigate));
+      console.log(resp)
+    // Reset
+    // setFormData({
+    //   username:"",
+    //   firstName: "",
+    //   lastName: "",
+    //   email: "",
+    //   password: "",
+    //   confirmPassword: "",
+    // })
+
+  }
+
+  return (
+    <div>
+      {/* Form */}
+      <form onSubmit={handleOnSubmit} className="flex w-full flex-col gap-y-4">
+        <div className="flex gap-x-4">
+          <label>
+            <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">
+              username<sup className="text-pink-200">*</sup>
+            </p>
+            <input
+              required
+              type="text"
+              name="username"
+              value={username}
+              onChange={handleOnChange}
+              placeholder="Enter username"
+              className="form-style w-full"
+            />
+          </label>
+          <label>
+            <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">
+              First Name <sup className="text-pink-200">*</sup>
+            </p>
+            <input
+              required
+              type="text"
+              name="firstName"
+              value={firstName}
+              onChange={handleOnChange}
+              placeholder="Enter first name"
+              className="form-style w-full"
+            />
+          </label>
+          <label>
+            <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">
+              Last Name <sup className="text-pink-200">*</sup>
+            </p>
+            <input
+              required
+              type="text"
+              name="lastName"
+              value={lastName}
+              onChange={handleOnChange}
+              placeholder="Enter last name"
+              className="form-style w-full"
+            />
+          </label>
+        </div>
+        <label className="w-full">
+          <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">
+            Email Address <sup className="text-pink-200">*</sup>
+          </p>
+          <input
+            required
+            type="text"
+            name="email"
+            value={email}
+            onChange={handleOnChange}
+            placeholder="Enter email address"
+            className="form-style w-full"
+          />
+        </label>
+        <div className="flex gap-x-4">
+          <label className="relative">
+            <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">
+              Create Password <sup className="text-pink-200">*</sup>
+            </p>
+            <input
+              required
+              type={showPassword ? "text" : "password"}
+              name="password"
+              value={password}
+              onChange={handleOnChange}
+              placeholder="Enter Password"
+              className="form-style w-full !pr-10"
+            />
+            <span
+              onClick={() => setShowPassword((prev) => !prev)}
+              className="absolute right-3 top-[38px] z-[10] cursor-pointer"
+            >
+              {showPassword ? (
+                <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
+              ) : (
+                <AiOutlineEye fontSize={24} fill="#AFB2BF" />
+              )}
+            </span>
+          </label>
+          <label className="relative">
+            <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">
+              Confirm Password <sup className="text-pink-200">*</sup>
+            </p>
+            <input
+              required
+              type={showConfirmPassword ? "text" : "password"}
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={handleOnChange}
+              placeholder="Confirm Password"
+              className="form-style w-full !pr-10"
+            />
+            <span
+              onClick={() => setShowConfirmPassword((prev) => !prev)}
+              className="absolute right-3 top-[38px] z-[10] cursor-pointer"
+            >
+              {showConfirmPassword ? (
+                <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
+              ) : (
+                <AiOutlineEye fontSize={24} fill="#AFB2BF" />
+              )}
+            </span>
+          </label>
+        </div>
+        <IconBtn
+          type="submit"
+          text={"Create Account"}
+        />
+        </form>
+    </div>
+  )
+}
+
+export default SignupForm
---
+++ server/index.js
@@ -0,0 +1,70 @@
+// Importing necessary modules and packages
+const path = require('path')
+const express = require("express");
+
+const cors = require("cors");
+const authRouter = require('./routes/auth.route');
+const postRouter = require('./routes/post.route');
+const commentRouter = require('./routes/comment.router');
+const userRouter = require('./routes/user.route');
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const { dbConnect } = require("./config/database");
+
+// Creating an Express application instance
+const app = express();
+// Loading environment variables from .env file
+dotenv.config();
+
+// Setting up port number
+const PORT = process.env.PORT ;
+
+
+// Connecting to database
+dbConnect();
+ 
+// Middleware setup
+
+// Parse JSON bodies of incoming requests
+app.use(express.json());
+
+// Parse cookies attached to incoming requests
+app.use(cookieParser());
+
+// Enable Cross-Origin Resource Sharing (CORS) for all origins with credentials support
+app.use(
+	cors({
+		origin: "*", // Allow requests from any origin
+		credentials: true, // Allow cookies and authorization headers
+	})
+);
+
+// Routing setup
+
+// Routing for authentication-related endpoints
+app.use('/api/v1/auth', authRouter);
+
+// Routing for post-related endpoints
+app.use('/api/v1/post', postRouter);
+
+// Routing for comment-related endpoints
+app.use('/api/v1/comment', commentRouter);
+
+// Routing for user-related endpoints
+app.use('/api/v1/user', userRouter);
+
+
+// Testing the server
+app.get("/", (req, res) => {
+	return res.json({
+		success: true,
+		message: "Your server is up and running ...",
+	});
+});
+
+// Listening to the server
+app.listen(PORT, () => {
+	console.log(`App is listening at ${PORT}`);
+});
+
+// End of code.
